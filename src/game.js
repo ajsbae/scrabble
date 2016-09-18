d2bb5d96ad92d1907a2fa7c3b37aa6e6a2d9864d
Alex Bae <ajsbae@gmail.com>
2016-09-18 11:36:09 -0400

    set up complete

--- src/game.js
+++ src/game.js
@@ -1,9 +1,31 @@
 /**
  * game.js 
  * @author Your Name <here@foo.bar.baz>
- */ 
+ */
 var tic = require('./tic-tac-toe.js');
 var readlineSync = require('readline-sync');
- 
-var answer = readlineSync.question('What is the meaning of life?');
-console.log(answer);
+
+
+
+
+
+console.log('Shall we play a game of TIC TAC TOE?')
+
+var rowSize = readlineSync.question('How wide should the board be? (1-26)');
+while (!rowSize < 27 && !rowSize > 0) {
+    rowSize = readlineSync.question('How wide should the board be? (1-26)');
+
+}
+
+var letter = readlineSync.question('Pick your letter: X or O');
+while (letter != 'X' && letter != 'O'){
+
+    letter = readlineSync.question('Pick your letter: X or O');
+
+}
+
+var board = tic.generateBoard(rowSize, rowSize, " ");
+tic.boardToString(board);
+
+
+while()
